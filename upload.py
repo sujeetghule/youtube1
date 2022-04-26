e7aef312857a21e936bd6fd8db521e91e62ae62c
sujeetghule <61055256+sujeetghule@users.noreply.github.com>
2022-04-26 13:10:34 +0530

    Update upload.py

--- upload.py
+++ upload.py
@@ -40,7 +40,7 @@ RETRIABLE_EXCEPTIONS = (httplib2.HttpLib2Error, IOError, httplib.NotConnected,
 
 RETRIABLE_STATUS_CODES = [500, 502, 503, 504]
 
-CLIENT_SECRETS_FILE = '/content/drive/MyDrive/CREDITIOAL.json'
+CLIENT_SECRETS_FILE = raw_input('Enter your client credential secret file path:\n')
 
 SCOPES = ['https://www.googleapis.com/auth/youtube.upload']
 
